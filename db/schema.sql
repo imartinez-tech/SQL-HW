7b480f6f39314fe52a9fbd2a88602fff75b8f0c7
ibraim martinez <ibraimm5@gmail.com>
2021-08-25 22:28:12 -0400

    adding schema file

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ USE work_db;
      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
      title VARCHAR(30) NOT NULL,
      salary DECIMAL NOT NULL,
-     department_id INT NOT NULL
+     department_id INT NOT NULL,
      FOREIGN KEY (department_id)
      REFERENCES department(id)
  );
@@ -23,8 +23,10 @@ USE work_db;
      last_name VARCHAR(30) NOT NULL, 
      role_id INT NOT NULL,
      manager_id INT NOT NULL, 
-     FOREIGN KEY (role_id),
-     FOREIGN KEY(manager_id),
-     REFERENCES role(id),
-     REFERENCES manager(id),
+     FOREIGN KEY (role_id)
+     REFERENCES role(id)
+     ON UPDATE CASCADE ON DELETE RESTRICT,
+     
+     FOREIGN KEY(manager_id)
+     REFERENCES role(id)
      );
